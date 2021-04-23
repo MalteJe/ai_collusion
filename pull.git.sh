5f41eb20c21477962c84b7240c84a58df9856fb5
MalteJe <malte.jeschonneck@uni-duesseldorf.de>
2021-04-23 20:50:11 +0200

    automating gcloud

---
+++ pull.git.sh
@@ -0,0 +1,6 @@
+# move into folder and pull latest version
+cd ai_collusion
+git pull
+
+# run script to manage experiment
+source run_experiment_on_gc.sh
---
+++ run_experiment_on_gc.sh
@@ -0,0 +1,29 @@
+# create log file
+export LOGFILE=shell.log
+
+
+# make directory for simulation results
+echo "creating shell log file" >> $LOGFILE
+mkdir -p "simulation_results"
+
+
+# invoke R script
+echo "invoking R script, writing R log file" >> $LOGFILE
+R CMD BATCH --slave master.R R-Output.Rout
+
+# move log files into simulation results
+echo "Rscript completed, retrieving lowest hierarchy folder for results" >> $LOGFILE
+SIM=`ls -d simulation_results/*/`
+echo "$SIM" >> $LOGFILE
+
+# move log files to results
+echo "moving R-Log and copying shell log to simulation results"
+mv R-Output.Rout $SIM
+cp $LOGFILE $SIM
+
+# upload simulation results to bucket
+echo "uploading to project bucket"
+gsutil -m cp -r simulation_results/* gs://ai_collusion/
+
+# shutdown VM
+sudo poweroff

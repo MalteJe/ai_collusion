e49fd923dac177e869d82f441a6b5218176c4833
MalteJe <malte.jeschonneck@uni-duesseldorf.de>
2021-04-24 17:08:05 +0200

    echo file on gc

--- run_experiment_on_gc.sh
+++ run_experiment_on_gc.sh
@@ -4,25 +4,31 @@ export LOGFILE=shell.log
 
 # make directory for simulation results
 echo "creating shell log file" >> $LOGFILE
+echo "creating shell log file"
 mkdir -p "simulation_results"
 
 
 # invoke R script
 echo "invoking R script, writing R log file" >> $LOGFILE
+echo "invoking R script, writing R log file"
 R CMD BATCH --slave master.R R-Output.Rout
 
 # move log files into simulation results
 echo "Rscript completed, retrieving lowest hierarchy folder for results" >> $LOGFILE
+echo "Rscript completed, retrieving lowest hierarchy folder for results"
 SIM=`ls -d simulation_results/*/`
 echo "$SIM" >> $LOGFILE
+echo "$SIM"
 
 # move log files to results
 echo "moving R-Log and copying shell log to simulation results" >> $LOGFILE
+echo "moving R-Log and copying shell log to simulation results"
 mv R-Output.Rout $SIM
 cp $LOGFILE $SIM
 
 # upload simulation results to bucket
 echo "uploading to project bucket" >> $LOGFILE
+echo "uploading to project bucket"
 gsutil -m cp -r simulation_results/* gs://ai_collusion/
 
 # shutdown VM

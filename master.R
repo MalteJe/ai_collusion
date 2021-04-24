57248b83e2b5b78438a219aa46364768311b5434
MalteJe <malte.jeschonneck@uni-duesseldorf.de>
2021-04-24 17:31:04 +0200

    adjusted alphas

--- master.R
+++ master.R
@@ -82,7 +82,7 @@ runs_per_experiment <- 4
 # Alpha -------------------------------------------------------------------
 
 
-alphas <- 1 * 10^-c(1, 3, 5, 7, 9)
+alphas <- 1 * 10^-c(3, 9)
 alpha_input <- list_modify(baseline, Alpha = alphas)
 
 print("defined specs, starting simulations")

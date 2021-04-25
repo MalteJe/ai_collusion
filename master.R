feff64ac901e16807c5d8dc41aa5ba7df8cdeca5
MalteJe <malte.jeschonneck@uni-duesseldorf.de>
2021-04-25 17:07:36 +0200

    next alphas

--- master.R
+++ master.R
@@ -82,7 +82,7 @@ runs_per_experiment <- 96
 # Alpha -------------------------------------------------------------------
 
 
-alphas <- 1 * 10^-c(1:2)
+alphas <- 1 * 10^-c(3:4)
 alpha_input <- list_modify(baseline, Alpha = alphas)
 
 print("defined specs, starting simulations")

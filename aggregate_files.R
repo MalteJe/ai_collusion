24dc6dde708be46486223488bcc9104f1a93b34c
MalteJe <malte.jeschonneck@uni-duesseldorf.de>
2021-05-05 14:13:30 +0200

    moved visualization from aggregating file to separate script

--- aggregate_files.R
+++ aggregate_files.R
@@ -27,6 +27,7 @@ load_return <- function(path, t_grouping, t_before_intervention = 8, t_profit = 
 			convergence = NA
 		))
 	} else {
+		print(path)
 		
 		load(path)
 		
@@ -80,7 +81,7 @@ experiment_job <- "Alpha_final"
 (path <- str_c("simulation_results/", experiment_job, "/"))
 filenames <- list.files(path) %>%
 	str_subset("RData$") %>%
-	str_subset("aggregated", negate = TRUE); print(head(filenames))
+	str_subset("aggregated", negate = TRUE); print(head(filenames)); print(length(filenames))
 
 adjusted_files <- str_replace(filenames, "poly_tiling", "poly-tiling")  %>%
 	str_replace("poly_separate", "poly-separate") %>%
@@ -210,171 +211,6 @@ ggsave("report/plots/convergence_at.png", width = 25, height = 15, units = "cm")
 
 
 
-
-
-
-
-# Vary Alpha --------------------------------------------------------------
-
-
-data %>%
-	group_by(feature_method, varied_parameter) %>%
-	summarize(avg_profits = mean(avg_profits, na.rm = TRUE)) %>%
-	mutate(Delta = get_delta(avg_profits)) %>%
-	ggplot(aes(x = as.double(varied_parameter), y = Delta, col = feature_method)) +
-	geom_line(size = 1) +
-	geom_point( size = 3) +
-	geom_hline(yintercept = c(0, 1)) +
-	theme_tq() +
-	scale_x_log10() +
-	labs(x = expression(alpha), y = expression(Delta)) +
-	color_dictionary
-
-filter(data, !is.na(avg_profits)) %>%
-	group_by(feature_method, varied_parameter) %>%
-	summarize(avg_profits = mean(avg_profits)) %>%
-	mutate(Delta = get_delta(avg_profits)) %>%
-	filter(Delta > 0) %>%
-	ggplot(aes(x = as.double(varied_parameter), y = Delta, col = feature_method)) +
-	geom_hline(yintercept = c(0, 1)) +
-	geom_line(size = 1) +
-	geom_point( size = 3) +
-	scale_y_continuous(expand = c(0, 0), breaks = seq(0,1, by = 0.25), labels = c(expression(Delta[n]), "0.25", "0.5", "0.75", expression(Delta[m]))) +
-	# theme_tq() +
-	scale_x_log10(minor_breaks = variations, breaks = variations) +
-	labs(x = expression(alpha), y = expression(Delta)) +
-	ai_theme +
-	color_dictionary +
-	theme(axis.title.y = element_text(size = 18, angle = 0, vjust = 0.5))
-ggsave("report/plots/alpha.png", width = 25, height = 15, units = "cm")
-
-
-filter(data, !is.na(avg_profits)) %>%
-	mutate(Delta = get_delta(avg_profits)) %>%
-	filter(Delta > 0) %>%
-	ggplot(aes(x = as.factor(varied_parameter), y = Delta, fill = feature_method)) +
-	geom_boxplot(position = "dodge") +
-	geom_hline(yintercept = c(0, 1)) +
-	scale_y_continuous(expand = c(0, 0), breaks = seq(0,1, by = 0.25), labels = c(expression(Delta[n]), "0.25", "0.5", "0.75", expression(Delta[m]))) +
-	# theme_tq() +
-	# scale_x_continuous(minor_breaks = variations, breaks = variations) +
-	xlab(experiment_job) +
-	labs(x = expression(alpha), y = expression(Delta)) +
-	ai_theme
-
-
-# Learning Phase Trajectory  ----------------------------------------------------------
-
-# experiment_ids <- 8; variations[experiment_ids]
-# 
-# 
-# 
-# outcomes <- data %>%
-# 	select(-intervention, -avg_profits) %>%
-# 	unnest(outcomes) %>%
-# 	filter(!is.na(t_group))
-
-manually_optimized_alpha <- data %>%
-	filter(
-		(feature_method == "poly-separated" & varied_parameter == "1e-06") |
-		(feature_method == "poly-tiling" & varied_parameter == "1e-12") |
-		(feature_method == "tiling" & varied_parameter == "0.001")  |
-		(feature_method == "tabular" & varied_parameter == "0.1")
-) 
-
-outcomes <- manually_optimized_alpha %>%
-	select(-intervention, -avg_profits) %>%
-	unnest(outcomes)
-
-
-
-
-learning_phase <- outcomes %>%
-	# complete(feature_method, varied_parameter, run_id, t_group) %>%
-	# group_by(feature_method, varied_parameter, run_id) %>%
-	# fill(price, Delta, .direction = "down") %>%
-	pivot_longer(cols = c(price, Delta), names_to = "metric") # %>%
-	# filter(varied_parameter %in% variations[experiment_ids])
-
-learning_phase %>%
-	mutate(p_n = ifelse(metric == "Delta", 0, 1.47),
-			 p_m = ifelse(metric == "Delta", 1, 1.93 )) %>%
-	ggplot(aes(x = t_group * t_grouping, y = value, group = interaction(feature_method, run_id, varied_parameter), col = feature_method)) +
-	geom_hline(aes(yintercept = p_n), linetype = "dashed") +	
-	geom_hline(aes(yintercept = p_m), linetype = "dashed") +	
-	geom_line() +
-	facet_wrap(~metric, ncol =1, scales = "free_y") +
-	theme_tq() +
-	ai_theme +
-	scale_x_continuous(labels = scales::comma) +
-	color_dictionary +
-	labs(x = "t", y = " ")
-ggsave("report/plots/all_runs.png", width = 25, height = 20, units = "cm")
-	
-
-learning_phase %>%
-	group_by(feature_method, varied_parameter, t_group, metric) %>%
-	summarize(value = mean(value, na.rm = TRUE)) %>%
-	mutate(p_n = ifelse(metric == "Delta", 0, 1.47),
-			 p_m = ifelse(metric == "Delta", 1, 1.93 )) %>%
-	ggplot(aes(x = t_group * t_grouping, y = value, color = feature_method)) +
-	geom_hline(aes(yintercept = p_n)) +	
-	geom_hline(aes(yintercept = p_m)) +	
-	geom_line(size = 1) +
-	facet_wrap(~ metric, ncol = 1, scales = "free_y") +
-	theme_tq()
-
-
-learning_phase %>%
-	ggplot(aes(x = as.factor(t_group * t_grouping), y = value, fill = feature_method)) +
-	geom_violin(position = "dodge", scale = "width") +
-	# geom_boxplot(position = "dodge") +
-	facet_wrap(~metric, ncol = 1, scales = "free_y") +
-	theme_tq() +
-	fill_dictionary
-	
-
-# Intervention
-
-
-
-intervention <- manually_optimized_alpha %>%
-	select(-outcomes, -avg_profits) %>%
-	unnest(intervention)
-
-
-
-intervention %>%
-	group_by(feature_method, varied_parameter, tau) %>%
-	summarize(price_1 = mean(price_1),
-				 price_2 = mean(price_2)) %>%
-	pivot_longer(cols = c("price_1", "price_2"), names_to = "price") %>%
-	ggplot(aes(x = tau, y = value, linetype = price, shape = price, col = feature_method)) +
-	geom_hline(yintercept = c(1.47, 1.93)) +
-	geom_vline(xintercept = 0, linetype = 2) +
-	geom_point(size = 3) +
-	geom_line(size = 1) +
-	theme_tq() +
-	color_dictionary +
-	facet_wrap(~feature_method, nrow = 2)
-ggsave("report/plots/average_intervention.png", width = 25, height = 20, units = "cm")
-
-intervention %>%
-	filter(tau > -1) %>%
-	group_by(feature_method, varied_parameter, run_id) %>%
-	transmute(price_change_1 = price_1 - first(price_1),
-			 price_change_2 = price_2 - first(price_2),
-			 tau = tau) %>%
-	pivot_longer(cols = c("price_change_1", "price_change_2"), names_to = "price") %>%
-	ggplot(aes(x = as.factor(tau), y = value, fill = feature_method)) +
-	geom_violin(scale = "width", trim = TRUE) +
-	# geom_boxplot() +
-	fill_dictionary +
-	facet_grid(feature_method~price) +
-	theme_tq() +
-	ai_theme +
-	labs(x = expression(tau))
-ggsave("report/plots/intervention_violin.png", width = 25, height = 25, units = "cm")
 
 
 
--- static_visualizations.R
+++ static_visualizations.R
@@ -1,355 +1,167 @@
-library(tidyquant)
-
-# replace runs with error recoveries with dummy run -----------------------
-
-load("simulation_results/res_varied_alpha.RData")
-
-dummy_run <- list(
-	outcomes = matrix(rep(NA_real_, 400), ncol = 4, dimnames = list(NULL, c("price_1", "price_2", "profit_1", "profit_2"))),
-	convergence = list(
-		converged = TRUE,
-		convergence_t = 2),
-	run_id = NA,
-	specs = list(
-		TT_intervention = 10,
-		convergence_cycle_length = 1
-	)
-)
+load("simulation_results/Alpha_final/aggregated.RData")
 
 
-
-replace_errors <- function(run) {
-	if(class(run) == "try-error") {
-		return(dummy_run)
-	} else {
-		return(run)
-	}
-}
-
-replace_errors_experiment <- function(experiment) {
-	map(experiment,
-		 replace_errors)
-}
-
-replace_errors_feature <- function(features_by) {
-	map(features_by,
-		 replace_errors_experiment)
-}
+# Vary Alpha --------------------------------------------------------------
 
 
-meta_meta_res <- map(meta_res_alpha,
-							replace_errors_feature)
+data %>%
+	group_by(feature_method, varied_parameter) %>%
+	summarize(avg_profits = mean(avg_profits, na.rm = TRUE)) %>%
+	mutate(Delta = get_delta(avg_profits)) %>%
+	ggplot(aes(x = as.double(varied_parameter), y = Delta, col = feature_method)) +
+	geom_line(size = 1) +
+	geom_point( size = 3) +
+	geom_hline(yintercept = c(0, 1)) +
+	theme_tq() +
+	scale_x_log10() +
+	labs(x = expression(alpha), y = expression(Delta)) +
+	color_dictionary
+
+filter(data, !is.na(avg_profits)) %>%
+	group_by(feature_method, varied_parameter) %>%
+	summarize(avg_profits = mean(avg_profits)) %>%
+	mutate(Delta = get_delta(avg_profits)) %>%
+	filter(Delta > 0) %>%
+	ggplot(aes(x = as.double(varied_parameter), y = Delta, col = feature_method)) +
+	geom_hline(yintercept = c(0, 1)) +
+	geom_line(size = 1) +
+	geom_point( size = 3) +
+	scale_y_continuous(expand = c(0, 0), breaks = seq(0,1, by = 0.25), labels = c(expression(Delta[n]), "0.25", "0.5", "0.75", expression(Delta[m]))) +
+	# theme_tq() +
+	scale_x_log10(minor_breaks = variations, breaks = variations) +
+	labs(x = expression(alpha), y = expression(Delta)) +
+	ai_theme +
+	color_dictionary +
+	theme(axis.title.y = element_text(size = 18, angle = 0, vjust = 0.5))
+ggsave("report/plots/alpha.png", width = 25, height = 15, units = "cm")
 
 
+filter(data, !is.na(avg_profits)) %>%
+	mutate(Delta = get_delta(avg_profits)) %>%
+	filter(Delta > 0) %>%
+	ggplot(aes(x = as.factor(varied_parameter), y = Delta, fill = feature_method)) +
+	geom_boxplot(position = "dodge") +
+	geom_hline(yintercept = c(0, 1)) +
+	scale_y_continuous(expand = c(0, 0), breaks = seq(0,1, by = 0.25), labels = c(expression(Delta[n]), "0.25", "0.5", "0.75", expression(Delta[m]))) +
+	# theme_tq() +
+	# scale_x_continuous(minor_breaks = variations, breaks = variations) +
+	xlab(experiment_job) +
+	labs(x = expression(alpha), y = expression(Delta)) +
+	ai_theme
 
 
-# trajectory of "main" specifications -------------------------------------
+# Learning Phase Trajectory  ----------------------------------------------------------
 
-get_convergence_t <- function(run) {
-	if (run$convergence$converged) {
-		return(run$convergence$convergence_t)
-	} else {
-		# return(run$specs$TT)
-		return(10000)
-	}
-}
+# experiment_ids <- 8; variations[experiment_ids]
+# 
+# 
+# 
+# outcomes <- data %>%
+# 	select(-intervention, -avg_profits) %>%
+# 	unnest(outcomes) %>%
+# 	filter(!is.na(t_group))
 
+manually_optimized_alpha <- data %>%
+	filter(
+		(feature_method == "poly-separated" & varied_parameter == "1e-06") |
+			(feature_method == "poly-tiling" & varied_parameter == "1e-12") |
+			(feature_method == "tiling" & varied_parameter == "0.001")  |
+			(feature_method == "tabular" & varied_parameter == "0.1")
+	) 
 
-
-get_delta <- function(profit) {
-	(profit - 0.2229272) / (0.3374905 - 0.2229272)
-}
-
-run_trajectory <- function(run, t_group) {
-	
-	conv_t <- get_convergence_t(run)
-	
-	if(is.na(conv_t)) {browser()}
-	
-	run$outcomes[1:conv_t,] %>%
-		as_tibble() %>%
-		transmute(t = row_number(),
-					 t_group = (t-1) %/% t_group + 1,
-					 price = (price_1 + price_2)/2 ,
-					 Delta = get_delta((profit_1 + profit_2)/2)) %>%
-		group_by(t_group) %>%
-		summarize_at(c("price", "Delta"), mean) %>%
-		mutate(run_id = run$run_id)
-	
-}
-
-
-experiment_trajectories <- function(feature_res, experiment_id, t_group) {
-	experiment <- feature_res[[experiment_id]]
-	
-	map_dfr(experiment,
-		 run_trajectory,
-		 t_group) %>%
-		complete(t_group, run_id) %>%
-		group_by(run_id) %>%
-		fill(price, Delta, .direction = "down") %>%
-		pivot_longer(cols = c(price, Delta), names_to = "metric")
-}
-
-# experiment_trajectories(meta_meta_res[[1]], 2, t_group = 1000) %>%
-# 	ggplot(aes(x = t_group * 1000, y = value, col = as.factor(run_id))) +
-# 	geom_line() +
-# 	facet_wrap(~metric, ncol = 1, scales = "free_y") +
-# 	theme_tq()
-
-
-
-average_trajectories <- function(feature_res, experiment_id, t_group) {
-	experiment_trajectories(feature_res, experiment_id, t_group) %>%
-		group_by(t_group, metric) %>%
-		summarize(value = mean(value))
-}
+outcomes <- manually_optimized_alpha %>%
+	select(-intervention, -avg_profits) %>%
+	unnest(outcomes)
 
 
 
 
+learning_phase <- outcomes %>%
+	# complete(feature_method, varied_parameter, run_id, t_group) %>%
+	# group_by(feature_method, varied_parameter, run_id) %>%
+	# fill(price, Delta, .direction = "down") %>%
+	pivot_longer(cols = c(price, Delta), names_to = "metric") # %>%
+# filter(varied_parameter %in% variations[experiment_ids])
 
-
-# Intervention ------------------------------------------------------------
-
-
-intervention_prices <- function(run, t_before_intervention) {
-	
-	tail(run$outcomes, run$specs$TT_intervention + t_before_intervention) %>%
-		as_tibble() %>%
-		select(price_1, price_2) %>%
-		mutate(run_id = run$run_id,
-				 tau = row_number() - t_before_intervention)
-}
-
-
-# intervention_prices(meta_meta_res[[3]][[3]][[2]], 10)
-
-
-intervention_experiment <- function(feature_res, experiment_id, t_before_intervention) {
-	map_dfr(feature_res[[experiment_id]],
-			  .f = intervention_prices,
-			  t_before_intervention = t_before_intervention)
-}
-
-
-intervention_avg_prices <- function(feature_res, experiment_id, t_before_intervention) {
-	intervention_experiment(feature_res, experiment_id, t_before_intervention) %>%
-		group_by(tau) %>%
-		summarize(price_1 = mean(price_1),
-					 price_2 = mean(price_2)) %>%
-		pivot_longer(cols = c("price_1", "price_2"), names_to = "price")
-}
-
-
-
-
-experiment_id <- 2
-
-map_dfr(.x = meta_meta_res,
-		  .f = experiment_trajectories,
-		  experiment_id = experiment_id,
-		  t_group = 1000,
-		  .id = "feature_method") %>%
-	ggplot(aes(x = t_group, y = value, group = interaction(feature_method, run_id), col = feature_method)) +
+learning_phase %>%
+	mutate(p_n = ifelse(metric == "Delta", 0, 1.47),
+			 p_m = ifelse(metric == "Delta", 1, 1.93 )) %>%
+	ggplot(aes(x = t_group * t_grouping, y = value, group = interaction(feature_method, run_id, varied_parameter), col = feature_method)) +
+	geom_hline(aes(yintercept = p_n), linetype = "dashed") +	
+	geom_hline(aes(yintercept = p_m), linetype = "dashed") +	
 	geom_line() +
 	facet_wrap(~metric, ncol =1, scales = "free_y") +
-	theme_tq()
+	theme_tq() +
+	ai_theme +
+	scale_x_continuous(labels = scales::comma) +
+	color_dictionary +
+	labs(x = "t", y = " ")
+ggsave("report/plots/all_runs.png", width = 25, height = 20, units = "cm")
 
 
-map_dfr(meta_meta_res,
-		  average_trajectories,
-		  experiment_id = experiment_id,
-		  t_group = 1000, .id = "feature_method") %>%
-	ungroup() %>%
-	complete(feature_method, t_group, metric) %>%
-	group_by(feature_method, metric) %>%
-	fill(value, .direction = "down") %>%
+learning_phase %>%
+	group_by(feature_method, varied_parameter, t_group, metric) %>%
+	summarize(value = mean(value, na.rm = TRUE)) %>%
 	mutate(p_n = ifelse(metric == "Delta", 0, 1.47),
-			 p_m = ifelse(metric == "Delta", 1, 1.97 )) %>%
-	ggplot(aes(x = t_group * 1000, y = value, color = feature_method)) +
+			 p_m = ifelse(metric == "Delta", 1, 1.93 )) %>%
+	ggplot(aes(x = t_group * t_grouping, y = value, color = feature_method)) +
 	geom_hline(aes(yintercept = p_n)) +	
 	geom_hline(aes(yintercept = p_m)) +	
 	geom_line(size = 1) +
 	facet_wrap(~ metric, ncol = 1, scales = "free_y") +
 	theme_tq()
 
-meta_meta_res[[3]][[2]][[1]]$outcomes
 
-# experiment_trajectories(meta_meta_res[[1]], 2, t_group = 10000) %>%
-# 	ggplot(aes(x = as.factor(t_group * 10000), y = value)) +
-# 	geom_boxplot() +
-# 	facet_wrap(~metric, ncol = 1, scales = "free_y")
-
-map_dfr(meta_meta_res, experiment_trajectories, experiment_id, t_group = 1000, .id = "feature_method") %>%
-	filter(t_group <= 10) %>%
-	ggplot(aes(x = as.factor(t_group), y = value, fill = feature_method)) +
-	geom_boxplot(position = "dodge") +
+learning_phase %>%
+	ggplot(aes(x = as.factor(t_group * t_grouping), y = value, fill = feature_method)) +
+	geom_violin(position = "dodge", scale = "width") +
+	# geom_boxplot(position = "dodge") +
 	facet_wrap(~metric, ncol = 1, scales = "free_y") +
-	theme_tq()
+	theme_tq() +
+	fill_dictionary
 
 
-map_dfr(meta_meta_res,
-		  .f = intervention_avg_prices,
-		  experiment_id = experiment_id,
-		  t_before_intervention = 8,
-		  .id = "feature_method") %>%
+# Intervention
+
+
+
+intervention <- manually_optimized_alpha %>%
+	select(-outcomes, -avg_profits) %>%
+	unnest(intervention)
+
+
+
+intervention %>%
+	group_by(feature_method, varied_parameter, tau) %>%
+	summarize(price_1 = mean(price_1),
+				 price_2 = mean(price_2)) %>%
+	pivot_longer(cols = c("price_1", "price_2"), names_to = "price") %>%
 	ggplot(aes(x = tau, y = value, linetype = price, shape = price, col = feature_method)) +
-	geom_hline(yintercept = c(1.47, 1.97)) +
+	geom_hline(yintercept = c(1.47, 1.93)) +
 	geom_vline(xintercept = 0, linetype = 2) +
 	geom_point(size = 3) +
 	geom_line(size = 1) +
 	theme_tq() +
-	facet_wrap(~feature_method)
+	color_dictionary +
+	facet_wrap(~feature_method, nrow = 2)
+ggsave("report/plots/average_intervention.png", width = 25, height = 20, units = "cm")
 
-# intervention_avg_prices(meta_meta_res[[1]], 4, 7) %>%
-# 	ggplot(aes(x = tau, y = value, linetype = price, shape = price)) +
-# 	geom_hline(yintercept = c(1.47, 1.97)) +
-# 	geom_point(size = 3) +
-# 	geom_line(size = 1) +
-# 	theme_tq()
-
-
-
-map_dfr(meta_meta_res,
-		  .f = intervention_experiment,
-		  experiment_id = experiment_id,
-		  t_before_intervention = 1, .id = "feature_method") %>%
-	group_by(feature_method, run_id) %>%
-	mutate(price_change_1 = price_1 - first(price_1),
-			 price_change_2 = price_2 - first(price_2)) %>%
+intervention %>%
+	filter(tau > -1) %>%
+	group_by(feature_method, varied_parameter, run_id) %>%
+	transmute(price_change_1 = price_1 - first(price_1),
+				 price_change_2 = price_2 - first(price_2),
+				 tau = tau) %>%
 	pivot_longer(cols = c("price_change_1", "price_change_2"), names_to = "price") %>%
-	ggplot(aes(x = as.factor(tau), y = value)) +
-	geom_boxplot() +
-	facet_grid(feature_method~price)
-
-
-
-# varying Alpha -----------------------------------------------------------
-
-
-avg_profits_upon_convergence <- function(run) {
-	conv_t <- get_convergence_t(run)
-	
-	# calculate average profits of maximally considered cycle length at convergence (before intervention)
-	run$outcomes[(conv_t - run$specs$convergence_cycle_length):conv_t,3:4] %>% mean()
-}
-
-
-
-
-experiment_avg_profits <- function(experiment_res) {
-	map_dbl(experiment_res, avg_profits_upon_convergence) %>% mean()
-}
-
-
-# alpha_avg_profits <- map_dbl(.x = meta_meta_res[[4]],
-# 										.f = experiment_avg_profits)
-
-
-feature_avg_profits <- function(feature_res) {
-	 map_dbl(.x = feature_res,
-	 		  .f = experiment_avg_profits)
-}
-
-
-# feature_avg_profits(meta_meta_res[[1]])
-
-
-avg_profits_varied_alpha <- map_dfc(.x = meta_meta_res,
-												.f = feature_avg_profits) %>%
-	mutate(alpha = alphas) %>%
-	pivot_longer(cols = features_extraction_methods, names_to = "feature_method", values_to = "avg_profit") %>%
-	mutate(Delta = get_delta(avg_profit))
-
-
-ggplot(avg_profits_varied_alpha, aes(x = alpha, y = Delta, col = feature_method)) +
-	geom_line(size = 1) +
-	geom_point( size = 3) +
-	geom_hline(yintercept = c(0, 1)) +
+	ggplot(aes(x = as.factor(tau), y = value, fill = feature_method)) +
+	geom_violin(scale = "width", trim = TRUE) +
+	# geom_boxplot() +
+	fill_dictionary +
+	facet_grid(feature_method~price) +
 	theme_tq() +
-	scale_x_log10()
+	ai_theme +
+	labs(x = expression(tau))
+ggsave("report/plots/intervention_violin.png", width = 25, height = 25, units = "cm")
 
 
 
-# Varying Lambda ----------------------------------------------------------
-
-avg_profits_varied_lambda <- map_dfc(.x = meta_res_lambda,
-												 .f = feature_avg_profits) %>%
-	mutate(lambda = lambdas) %>%
-	pivot_longer(cols = features_extraction_methods, names_to = "feature_method", values_to = "avg_profit") %>%
-	mutate(Delta = get_delta(avg_profit))
-
-
-ggplot(avg_profits_varied_lambda, aes(x = lambda, y = Delta, col = feature_method)) +
-	geom_line(size = 1) +
-	geom_point( size = 3) +
-	geom_hline(yintercept = c(0, 1)) +
-	theme_tq()
-
-
-
-# Varying Delta -----------------------------------------------------------
-
-avg_profits_varied_delta <- map_dfc(.x = meta_res_delta,
-											 .f = feature_avg_profits) %>%
-	mutate(delta = delta_input$Delta) %>%
-	pivot_longer(cols = features_extraction_methods, names_to = "feature_method", values_to = "avg_profit") %>%
-	mutate(Delta = get_delta(avg_profit))
-
-
-ggplot(avg_profits_varied_delta, aes(x = delta, y = Delta, col = feature_method)) +
-	geom_line(size = 1) +
-	geom_point( size = 3) +
-	geom_hline(yintercept = c(0, 1)) +
-	theme_tq()
-
-
-
-# Varying Psi -------------------------------------------------------------
-
-avg_profits_varied_psi <- map_dfc(.x = meta_res_psi,
-												.f = feature_avg_profits) %>%
-	mutate(psi = psi_input$Psi) %>%
-	pivot_longer(cols = features_extraction_methods, names_to = "feature_method", values_to = "avg_profit") %>%
-	mutate(Delta = get_delta(avg_profit))
-
-
-ggplot(avg_profits_varied_psi, aes(x = psi, y = Delta, col = feature_method)) +
-	geom_line(size = 1) +
-	geom_point( size = 3) +
-	geom_hline(yintercept = c(0, 1)) +
-	theme_tq()
-
-
-
-# Varying Zeta -------------------------------------------------------------
-
-avg_profits_varied_zeta <- map_dfc(.x = meta_res_zeta,
-											 .f = feature_avg_profits) %>%
-	mutate(zeta = zeta_input$zeta) %>%
-	pivot_longer(cols = features_extraction_methods, names_to = "feature_method", values_to = "avg_profit") %>%
-	mutate(Delta = get_delta(avg_profit))
-
-
-ggplot(avg_profits_varied_zeta, aes(x = zeta, y = Delta, col = feature_method)) +
-	geom_line(size = 1) +
-	geom_point( size = 3) +
-	geom_hline(yintercept = c(0, 1)) +
-	theme_tq()
-
-
-# Varying m -------------------------------------------------------------
-
-avg_profits_varied_m <- map_dfc(.x = meta_res_m,
-											  .f = feature_avg_profits) %>%
-	mutate(m = m_input$m) %>%
-	pivot_longer(cols = features_extraction_methods, names_to = "feature_method", values_to = "avg_profit") %>%
-	mutate(Delta = get_delta(avg_profit))
-
-
-ggplot(avg_profits_varied_m, aes(x = m, y = Delta, col = feature_method)) +
-	geom_line(size = 1) +
-	geom_point( size = 3) +
-	geom_hline(yintercept = c(0, 1)) +
-	theme_tq()

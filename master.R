0af5a842202e23b2dacddfaa14ecc881a8487606
MalteJe <malte.jeschonneck@uni-duesseldorf.de>
2021-05-11 14:16:26 +0200

    run on gcp with more memory

--- master.R
+++ master.R
@@ -36,6 +36,8 @@ print("defining specs")
 
 # methods
 features_extraction_methods <- c("tabular", "tiling", "poly_separated", "poly_tiling")
+features_extraction_methods <- "poly_tiling"
+
 
 # static specs (no variation in study whatsoever)
 static_specs <- list(
@@ -100,6 +102,7 @@ baseline <- list(
 
 
 alphas_manually_optimized <- c(0.1, 0.001, 1 * 10^-6, 1 * 10^-8)
+alphas_manually_optimized <- 10^-8
 
 
 
@@ -135,17 +138,16 @@ alphas_manually_optimized <- c(0.1, 0.001, 1 * 10^-6, 1 * 10^-8)
 
 # # Delta -------------------------------------------------------------------
 
-deltas <- c(0L, 0.25, 0.5, 0.75, 0.9, 1L)
-deltas <- c(0.75, 0.9, 1L)
-delta_input <- list_modify(baseline, Alpha = NULL, Delta = deltas)
-
-walk2(.x = features_extraction_methods,
-		.y = alphas_manually_optimized,
-		.f = vary_parameter,
-		variable_specs = delta_input,
-		static_specs = static_specs,
-		runs = runs_per_experiment,
-		no_of_cores = no_of_cores)
+# deltas <- c(0L, 0.25, 0.5, 0.75, 0.9, 1L)
+# delta_input <- list_modify(baseline, Alpha = NULL, Delta = deltas)
+# 
+# walk2(.x = features_extraction_methods,
+# 		.y = alphas_manually_optimized,
+# 		.f = vary_parameter,
+# 		variable_specs = delta_input,
+# 		static_specs = static_specs,
+# 		runs = runs_per_experiment,
+# 		no_of_cores = no_of_cores)
 
 
 
